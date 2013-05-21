46aa55b196f46d3913fbb8c5d45bcf4ce9229fe3
Ludovic Fardel <ludovic.fardel@gmail.com>
2013-05-21 13:51:41 +0200

    initial commit

---
+++ public/javascript/badges.js
@@ -0,0 +1,8 @@
+YUI({
+    //Last Gallery Build of this module
+        gallery: 'gallery-2011.09.28-20-06',
+        skin: 'night' 
+      }).use('gallery-yui-tooltip', function(Y) {
+          var tooltip = new Y.Tooltip();
+            tooltip.render();
+      });
